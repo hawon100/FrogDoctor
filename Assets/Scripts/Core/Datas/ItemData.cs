6c0ad19af0086a18f2ece628766b66545258ef06
hawon <hawonoj002@gmail.com>
2024-01-15 16:10:37 +0900

    ItemBase

---
+++ Assets/Scripts/Core/Datas/ItemData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New ItemData", menuName = "Data/ItemData", order = int.MinValue)]
+public class ItemData : BaseData
+{
+
+}
